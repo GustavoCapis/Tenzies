dfd204fd6ef58dab5dbc42367561a9a439e729ea
Gustavo Nepomuceno Capistrano <gustavo.capy@gmail.com>
2026-01-26 14:46:39 -0300

    feat: Add Die component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,19 @@
+import Die from "./Die.jsx";
 export default function App() {
-  return <main></main>;
+  return (
+    <main>
+      <div className="die-container">
+        <Die value="3" />
+        <Die value="3" />
+        <Die value="3" />
+        <Die value="3" />
+        <Die value="3" />
+        <Die value="3" />
+        <Die value="3" />
+        <Die value="3" />
+        <Die value="3" />
+        <Die value="3" />
+      </div>
+    </main>
+  );
 }
