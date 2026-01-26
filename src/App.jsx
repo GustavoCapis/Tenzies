6bd3c3e59368247422afd86058e168ad360c9436
Gustavo Nepomuceno Capistrano <gustavo.capy@gmail.com>
2026-01-26 15:12:58 -0300

    feat: Add function to generate 10 random numbers.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
 import Die from "./Die.jsx";
 export default function App() {
+  //returns an array of 10 random numbers between 1-6 inclusive.
+  function generateAllNewDice() {
+    const diceArray = [];
+
+    for (let i = 0; i < 10; i++) {
+      const randomNumber = Math.floor(Math.random() * 7) + 1;
+      diceArray.push(randomNumber);
+    }
+
+    return diceArray;
+  }
+
   return (
     <main>
       <div className="die-container">
