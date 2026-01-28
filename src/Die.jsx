19b3485fd242f53056bd090abc0fe5659957fc91
Gustavo Nepomuceno Capistrano <gustavo.capy@gmail.com>
2026-01-28 10:41:14 -0300

    feat: Add accessibility improvements.

--- src/Die.jsx
+++ src/Die.jsx
@@ -2,7 +2,13 @@ export default function Die(props) {
   const className = props.isHeld ? "is-held" : "dice-button";
 
   return (
-    <button className={className} onClick={() => props.onHold(props.id)}>
+    <button
+      className={className}
+      onClick={() => props.onHold(props.id)}
+      aria-pressed={props.isHeld}
+      aria-label={`Die with value ${props.value}, 
+            ${props.isHeld ? "held" : "not held"}`}
+    >
       {props.value}
     </button>
   );
